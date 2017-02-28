146d25aabb73af6ac08ef7895137397dc5b70019
Patrick <farrellryan@comcast.net>
2017-02-28 12:09:24 -0800

    tourney

---
+++ cogs/pickorder.py
@@ -0,0 +1,30 @@
+import discord
+import random
+#from discord.ext import commands
+
+class pickorder:
+
+	def __init__(self, bot):
+		self.bot = bot
+
+	@commands.command(pass_context=True)
+	async def pickorder(self, ctx, message):
+ 
+		id = message.split( )
+
+		i=1
+		numbers=[];
+
+		for name in id 
+			await self.bot.say(i + ". " + name)
+			numbers.append(i)
+			i+=1
+
+		random.shuffle(id)
+
+		for num in numbers 
+			await self.bot.say(i + ". " + name)
+			numbers.append(i)
+
+	def setup(bot):
+		bot.add_cog(pickorder(bot))
