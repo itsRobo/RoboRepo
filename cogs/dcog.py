b9e25f549f56190c07611b2edda82ea876d19b53
Patrick <farrellryan@comcast.net>
2017-02-28 21:54:08 -0800

    dcog fix 19

--- cogs/dcog.py
+++ cogs/dcog.py
@@ -13,12 +13,13 @@ class dcog:
 
 		triggerSubtext = "/id/" #this is the string directly before the id in the url
 		discordID = user.id
+		await self.bot.say("Your ID: " + str(discordID) )
 
 		if( "http://steamcommunity.com/id" in steamID ):
 			steamID = steamID[steamID.find(triggerSubtext)+len(triggerSubtext):]
 
 		if os.path.isfile("data/mmr/mmr.txt"):
-			file = open( "data/mmr/mmr.txt", "a+")
+			file = open( "data/mmr/mmr.txt", "a")
 			file.write( str( discordID ) + "linksto " + str(steamID) + "\n")
 			await self.bot.say("Success!")
 		else:
