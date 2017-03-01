94c39cd75df112b5aa63f05f195bbd4d5f509982
Patrick <farrellryan@comcast.net>
2017-02-28 21:39:30 -0800

    dcog fix 16

--- cogs/dcog.py
+++ cogs/dcog.py
@@ -12,14 +12,14 @@ class dcog:
 	async def linkmmr(self, ctx, steamID, user : discord.User):
 
 		triggerSubtext = "/id/" #this is the string directly before the id in the url
-		steamid = steamID
+		discordID = user.id
 
 		if( "http://steamcommunity.com/id" in steamID ):
-			steamid = steamid[steamid.find(triggerSubtext)+len(triggerSubtext):]
+			steamID = steamID[steamID.find(triggerSubtext)+len(triggerSubtext):]
 
 		if os.path.isfile("data/mmr/mmr.txt"):
 			file = open( "data/mmr/mmr.txt", "a")
-			file.write( str( user.id ) + "linksto " + str(steamid) )
+			file.write( str( discordID ) + "linksto " + str(steamID) + "\n")
 			await self.bot.say("Success!")
 		else:
 			await self.bot.say("Something went wrong!")
