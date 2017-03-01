df19a26d258b4a376c7a608c66387e67db7e30de
Patrick <farrellryan@comcast.net>
2017-02-28 23:45:05 -0800

    linkmmr fix

--- cogs/mmr.py
+++ cogs/mmr.py
@@ -22,14 +22,14 @@ class mmr:
 			steamID = steamID[steamID.find(triggerSubtextTwo)+len(triggerSubtextTwo):]
 
 		if os.path.isfile("data/mmr/mmr.txt"):
-			txt = open( "data/mmr/mmr.txt", "r")
+			txt = open( "data/mmr/mmr.txt", "r").read()
 			
 
 			await self.bot.say("Steam ID is " + steamID)
 			await self.bot.say("Steam ID is " + discordID)
 			await self.bot.say("txt is" + str(txt) )
 
-			if ( (steamID in str(txt) or (discordID in str(txt) ):
+			if ( (steamID in txt) or (discordID in txt):
 				await self.bot.say("Your discord / Steam ID was already registerd")
 			else:
 				file = open( "data/mmr/mmr.txt", "a")
