d6b07c2934b13ea840b55c6423f6d1e6af0d213e
Patrick <farrellryan@comcast.net>
2017-02-28 20:54:31 -0800

    dcog fix 8

--- cogs/dcog.py
+++ cogs/dcog.py
@@ -18,7 +18,7 @@ class dcog:
 			id = id[id.find(triggerSubtext)+len(triggerSubtext):]
 
 		#if os.path.isfile("data/mmr/mmr.txt"):
-		file = open( "data/mmr/mmr.txt", a)
+		file = open( "data/mmr/mmr.txt", "a")
 		file.write( discord.member + "linksto" + id)
 		await self.bot.say("Success!")
 		#else:
