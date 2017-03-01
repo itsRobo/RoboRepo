8fa34db3eeeaebccea7e532696f6c6377c6dc938
Patrick <farrellryan@comcast.net>
2017-02-28 23:39:43 -0800

    linkmmr fix

--- cogs/mmr.py
+++ cogs/mmr.py
@@ -26,8 +26,9 @@ class mmr:
 			
 
 			await self.bot.say("Steam ID is " + steamID)
-			await self.bot.say("Steam ID is " + disocordID)
+			await self.bot.say("Steam ID is " + discordID)
 			await self.bot.say("txt is" + txt)
+			
 			if ( (steamID in txt) or (discordID in txt) ):
 				await self.bot.say("Your discord / Steam ID was already registerd")
 			else:
